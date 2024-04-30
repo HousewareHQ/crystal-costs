5449bef392fe223ec44045bb8058fecf5779b684
Shubham Pandey <shubhampcpandey13@gmail.com>
2024-04-30 17:53:30 +0530

    wip: checkpointed

---
+++ models/response.py
@@ -0,0 +1,12 @@
+from typing import List, Dict, Any
+
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+
+class Response(BaseModel):
+    """Final response to the question being asked"""
+    answer: str = Field(description = "The final answer to respond to the user")
+    summary: str = Field(description = "This should be a concise summary based on the time-series data")
+    data: List[Dict[str, Any]] = Field(description = "If the final answer has a time series data, then it should be formatted here, else this can be empty list")
+    # data: {}[] time series
+    # summary: summary of chart
