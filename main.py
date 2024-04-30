fd3e31fe0eda9e4e63b2639883a449003bad4669
priyanshu-09 <priyanshushukla9801@gmail.com>
2024-04-30 16:08:54 +0530

    Basic Streamlit chat experience

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from langchain_community.agent_toolkits import create_sql_agent
 from langchain_openai import ChatOpenAI
 from langchain_community.utilities import SQLDatabase
-
+from langchain_community.callbacks.streamlit import (
+    StreamlitCallbackHandler,
+)
 import os
+import streamlit as st
 
 
 username = os.environ["SNOWFLAKE_USERNAME"]
@@ -13,13 +16,44 @@ schema = os.environ["SNOWFLAKE_SCHEMA"]
 warehouse = os.environ["SNOWFLAKE_WAREHOUSE"]
 role = os.environ["SNOWFLAKE_ROLE"]
 
+st.title("CrystalCosts")
+st.write('Get accurate cost analysis using Natural Language')
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Hello! How can I help you today?"}]
+
+
 snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
 db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1, include_tables=['query_history','warehouse_metering_history'], view_support=True)
 
 
-llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, streaming=True)
 
 agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True)
-agent_executor.invoke(
-    "How many credits were burned in the last 24 hours?"
-)
+
+
+
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+if prompt := st.chat_input("What is up?"):
+   
+    st.chat_message("user").markdown(prompt)
+    
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # response = agent_executor.invoke(prompt)
+    
+    with st.chat_message("assistant"):
+        st_callback = StreamlitCallbackHandler(st.container())
+        response = agent_executor.invoke(
+            {"input": prompt}, {"callbacks": [st_callback]}
+        )
+        
+        st.markdown(response['output'])
+        st.session_state.messages.append({"role": "assistant", "content": response['output']})
+        
+
